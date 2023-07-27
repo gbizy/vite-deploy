7e64d4e118e7a5f01c3d3fd54a37644dcff11959
unknown <gbenyis1@binghamton.edu>
2023-07-26 22:50:52 -0400

    altered react-router implementation. website now ready to deploy on main ghp

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import MyWork from "./pages/MyWork";
-import Contact from "./pages/Contact";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/mywork" element={<MyWork />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <>
+      <Outlet />
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,39 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import MyWork from "./pages/MyWork.tsx";
+import Contact from "./pages/Contact.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/vite-deploy/",
+    element: <App />,
+    children: [
+      {
+        path: "/vite-deploy/",
+        element: <Home />,
+      },
+      {
+        path: "/vite-deploy/about",
+        element: <About />,
+      },
+      {
+        path: "/vite-deploy/mywork",
+        element: <MyWork />,
+      },
+      {
+        path: "/vite-deploy/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
